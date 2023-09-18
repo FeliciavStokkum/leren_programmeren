7ccb7bfc00fd091f2c7e9299e9d00c2607598498
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-09-18 15:11:43 +0200

    speelhal_2.0

---
+++ Module_1/deel_1/speelhal_2.0.py
@@ -0,0 +1,8 @@
+toegangsticket = 7.45 * 4
+vr_per_persoon = 0.37 * 9 
+aantal_personen = 4
+aantal_minuten_vr = 45
+
+totaal = toegangsticket + vr_per_persoon
+
+print(f"Dit geweldige dagje-uit met {aantal_personen} mensen in de Speelhal met {aantal_minuten_vr} minuten VR kost je maar {totaal} euro")
