92b8d45f4ad0f368958d5ff2cf2c81e76e734aff
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-09-20 09:58:19 +0200

    person

---
+++ Module_1/deel_2/person.py
@@ -0,0 +1,17 @@
+# Maak een programma person.py dat de gebruiker om zijn naam, adres,
+# postcode en woonplaats vraagt met duidelijke prompts.
+
+# Laat het programma daarna een adreskaart (met lijntjes) tonen in de terminal:
+
+
+naam = input("Vul hier je naam in: ")
+adres = input("Vul hier je adres in: ")
+postcode = input("Vul hier je postcode in: ")
+woonplaats = input("Vul hier je woonplaats in: ")
+
+print("-----------------------------------------------------------------")
+print(f"| naam: {naam}")  
+print(f"| adres: {adres}")  
+print(f"| postcode: {postcode}")  
+print(f"| woonplaats: {woonplaats}")  
+print("-----------------------------------------------------------------")
