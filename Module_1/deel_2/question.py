16bf4d71a5b67c3eb2ec6a3e77a5af34b4ad41ad
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-09-20 10:30:24 +0200

    question

---
+++ Module_1/deel_2/question.py
@@ -0,0 +1,7 @@
+naam = input("Hallo, wat is je naam? ")
+leeftijd = input(f"Goedendag {naam}, wat is je leeftijd? ")
+leeftijd = int(leeftijd)
+eten = input(f"En als {leeftijd} jarige, wat eet je het liefst? ")
+drinken = input(f"En wat drink je het liefst bij {eten}? ")
+
+print(f"De {leeftijd} jarige {naam} drinkt het liefs {drinken} bij {eten}")
