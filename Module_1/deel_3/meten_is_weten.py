267da23e101d0b62f5568fc567b5e2e24d774f82
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-10-11 14:30:31 +0200

    input en if-statement

---
+++ Module_1/deel_3/meten_is_weten.py
@@ -0,0 +1,10 @@
+#Laat de gebruiker 2 hele getallen invoeren
+a = input("Voer getal a in: ")
+b = input("Voer getal b in: ")
+max = a
+
+#Bepaal welk getal groter is dmv een if statement
+if a > b: 
+    print("a is het grootste getal:", max)
+elif b > a:
+    print("getal b is groter dan getal a")
