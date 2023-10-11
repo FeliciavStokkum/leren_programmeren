a30a681e67e8dc7e5bfb487eb601d51c7d61430f
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-10-11 14:32:55 +0200

    elif-statement

--- Module_1/deel_3/meten_is_weten.py
+++ Module_1/deel_3/meten_is_weten.py
@@ -1,9 +1,11 @@
 #Laat de gebruiker 2 hele getallen invoeren
 a = input("Voer getal a in: ")
 b = input("Voer getal b in: ")
-max = a
 
 #Bepaal welk getal groter is dmv een if statement
 if a > b: 
+    max = a
     print("a is het grootste getal:", max)
-
+elif a < b:
+    min = a
+    print("a is het kleinste getal:", min)
