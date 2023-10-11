4343ae46fe753d91488a3b5cefaedd2558f9ccf8
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-10-11 14:30:14 +0200

    pizza

--- Module_1/deel_2/pizzaCalculator.py
+++ Module_1/deel_2/pizzaCalculator.py
@@ -12,22 +12,22 @@ aantal_medium_pizza = int(input("Hoeveel medium pizza's wil je? "))
 aantal_large_pizza = int(input("Hoeveel large pizza's wil je? "))
 
 #prijzen voor de pizza's
-PRIJS_SMALL = aantal_small_pizza * SMALL_PIZZA
-PRIJS_MEDIUM = aantal_medium_pizza * MEDIUM_PIZZA
-PRIJS_LARGE = aantal_large_pizza * LARGE_PIZZA
-TOTAAL_PRIJS_PIZZA = (PRIJS_SMALL + PRIJS_MEDIUM + PRIJS_LARGE)
+prijs_small = aantal_small_pizza * SMALL_PIZZA
+prijs_medium = aantal_medium_pizza * MEDIUM_PIZZA
+prijs_large = aantal_large_pizza * LARGE_PIZZA
+totaal_prijs_pizza = (prijs_small + prijs_medium + prijs_large)
 
 #Totaa; prijs 
-bestelde_pizza_small = print(f"Je hebt {aantal_small_pizza} kleine pizza's besteld, dit kost {PRIJS_SMALL}")
-bestelde_pizza_medium = print(f"Je hebt {aantal_medium_pizza} medium pizza's besteld, dit kost {PRIJS_MEDIUM}")
-bestelde_pizza_large = print(f"Je hebt {aantal_large_pizza} grote pizza's besteld, dit kost {PRIJS_LARGE}")
-TOTAAL_PRIJS_PIZZA = print(f"De totaal prijs voor alles pizza's is: {TOTAAL_PRIJS_PIZZA}")
+bestelde_pizza_small = print(f"Je hebt {aantal_small_pizza} kleine pizza's besteld, dit kost {prijs_small}")
+bestelde_pizza_medium = print(f"Je hebt {aantal_medium_pizza} medium pizza's besteld, dit kost {prijs_medium}")
+bestelde_pizza_large = print(f"Je hebt {aantal_large_pizza} grote pizza's besteld, dit kost {prijs_large}")
+totaal_prijs_pizza = print(f"De totaal prijs voor alles pizza's is: {totaal_prijs_pizza}")
 
 #Bon
 print("Hier is uw bon")
 print("-----------------------------------------------")
-print(f"|  Je hebt totaal {aantal_small_pizza}  kleine pizza's. Dit kost: {PRIJS_SMALL}")
-print(f"|  Je hebt totaal {aantal_medium_pizza}  medium pizza's. Dit kost: {PRIJS_MEDIUM}")
-print(f"|  Je hebt totaal {aantal_large_pizza}  grote pizza's.  Dit kost: {PRIJS_LARGE}")
-print(f"|  Je totaal is                              : {PRIJS_SMALL + PRIJS_MEDIUM + PRIJS_LARGE}")
+print(f"|  Je hebt totaal {aantal_small_pizza}  kleine pizza's. Dit kost: {prijs_small}")
+print(f"|  Je hebt totaal {aantal_medium_pizza}  medium pizza's. Dit kost: {prijs_medium}")
+print(f"|  Je hebt totaal {aantal_large_pizza}  grote pizza's.  Dit kost: {prijs_large}")
+print(f"|  Je totaal is                              : {prijs_small + prijs_medium + prijs_large}")
 print("------------------------------------------------")
