a6489399de357f6412ad1716336e7ca9b00fbfa1
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-10-17 16:55:19 +0200

    Min en Max

--- Module_1/deel_3/meten_is_weten.py
+++ Module_1/deel_3/meten_is_weten.py
@@ -5,9 +5,15 @@ b = input("Voer getal b in: ")
 #Bepaal welk getal groter is dmv een if statement
 if a > b: 
     max = a
+    min = b
     print("a is het grootste getal:", max)
+    print("Het minimum is", min)
+    print("Het maximum is", max)
 elif a < b:
     min = a
+    max = b
     print("a is het kleinste getal:", min)
+    print("Het minimum is", min)
+    print("Het maximum is", max)
 else: 
     print("a en b zijn even groot")
