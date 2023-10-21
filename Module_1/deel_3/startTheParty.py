edd00a88ac82be8b35941c6e1009f0ee884295b0
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-10-21 15:05:39 +0200

    startTheParty.py

--- Module_1/deel_3/startTheParty.py
+++ Module_1/deel_3/startTheParty.py
@@ -1,9 +1,11 @@
-gastheer = True
+gastheer = input("Wie is de gastheer? ")
 gasten = True
 drank = False
 chips = True
+NAAM = "Felicia"
+SLB = "Jouke"
 
-if chips and drank or gasten and chips and drank or gastheer and drank or gastheer and gasten and chips and drank:
+if chips and drank or gasten and chips and drank or gastheer and drank or gastheer and gasten and chips and drank and gastheer == NAAM or gastheer == SLB:
     print('Start the Party')
 else:
     print('No Party')
