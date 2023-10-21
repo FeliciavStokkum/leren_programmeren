57fccc13066ae071b63ad680ff06dad10009012b
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-10-21 15:08:33 +0200

    startTheParty.py

--- Module_1/deel_3/startTheParty.py
+++ Module_1/deel_3/startTheParty.py
@@ -1,11 +1,11 @@
 gastheer = input("Wie is de gastheer? ")
-gasten = True
+gasten = int(input("Hoeveel gasten komen er? "))
 drank = False
 chips = True
 NAAM = "Felicia"
 SLB = "Jouke"
 
-if chips and drank or gasten and chips and drank or gastheer and drank or gastheer and gasten and chips and drank and gastheer == NAAM or gastheer == SLB:
+if chips and drank or gasten and chips and drank or gastheer and drank or gastheer and gasten and chips and drank and gastheer == NAAM or gastheer == SLB or gasten >= 4 and gasten <20:
     print('Start the Party')
 else:
     print('No Party')
