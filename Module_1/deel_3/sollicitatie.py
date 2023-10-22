8476d9daef50aa74ffef92f7d99b0d94195279ba
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-10-22 11:30:22 +0200

    Sollicitatie.py

--- Module_1/deel_3/sollicitatie.py
+++ Module_1/deel_3/sollicitatie.py
@@ -6,6 +6,16 @@ certificaat = input("Ben je in het bezit van certificaat: Overleven met gevaarli
 dieren_dressur = int(input("Hoeveel jaren praktijkervaring heb je met dieren-dressuur? "))
 jongleren = int(input("Hoeveel jaren ervaring heb je met jongleren? "))
 acrobatiek = int(input("Hoeveel jaren praktijkervaring heb je met acrobatiek? "))
+mbo_4 = input("Ben je in het bezit van een MBO-4 diploma ondernemen? Y/N ").lower()
+ondernemer = int(input("Hoelang ben u een ondernemer, vul in hele jaren in: "))
+werknemer = int(input("Hoeveel werknemers heeft u? "))
+man = input("bent u een man? Y/N ")
+snor = int(input("Hoe breed is uw snor? Vul in hele getallen in: "))
+vrouw = input("Bent u een vrouw? ")
+haar_kleur = input("Wat is uw haarkleur? ")
+haar_lengte = input ("Hoelang is uw haar? ")
+anders = input("Is uw gender niet man of vrouw? Y/N")
+glimlach = int(input("Hoe breed is uw glimlach? "))
 
 MAX_WEIGHT = 120
 MIN_WEIGHT = 90
@@ -17,7 +27,65 @@ MIN_DIEREN = 4
 MIN_JONGLEREN = 5
 MIN_ACROBATIEK = 3
 
-if (rijbewijs == "y" and hoed == "y" and certificaat == "y" and lichaamsgewicht >= MIN_WEIGHT and lichaamsgewicht <= MAX_WEIGHT and lengte >= MIN_HEIGHT and lengte <= MAX_HEIGHT and dieren_dressur >= MIN_DIEREN and jongleren >= MIN_JONGLEREN and acrobatiek >= MIN_ACROBATIEK):
+MIN_ONDERNEMER = 3
+MIN_WERKNEMERS = 5
+
+SNOR_BREED = 10
+GLIMLACH = 10
+
+rijbewijs_ok = rijbewijs == "y"
+hoed_ok = hoed == "y"
+lichaamsgewicht_ok = lichaamsgewicht >= MIN_WEIGHT
+lengte_ok = lengte >= MIN_HEIGHT
+certificaat_ok = certificaat == "y"
+dieren_dressur_ok = dieren_dressur >= MIN_DIEREN
+jongleren_ok = jongleren >= MIN_JONGLEREN
+acrobatiek_ok = acrobatiek >= MIN_ACROBATIEK
+mbo_4_ok = mbo_4 == "y"
+ondernemer_ok = ondernemer >= MIN_ONDERNEMER
+werknemer_ok = werknemer >= MIN_WERKNEMERS
+man_ok = man == "y"
+snor_ok = snor >= SNOR_BREED
+vrouw_ok = vrouw == "y"
+haar_kleur_ok = haar_kleur == "rood"
+haar_lengte_ok = haar_lengte >= 20
+anders_ok = anders == "y"
+glimlach_ok = glimlach >= GLIMLACH
+
+
+if (rijbewijs_ok and hoed_ok and certificaat_ok and lichaamsgewicht_ok and lengte_ok and
+    dieren_dressur_ok and jongleren_ok and acrobatiek_ok and
+    mbo_4_ok and ondernemer_ok and werknemer_ok and
+    snor_ok and haar_kleur_ok and haar_lengte_ok and glimlach_ok):
     print("Gefeliciteerd, u komt in aanmerking voor een sollicitatiegesprek.")
 else:
-    print("Helaas, je bent niet door de keuring heen.")
+    if not rijbewijs_ok:
+        print("U heeft geen geldig rijbewijs")
+    if not hoed_ok:
+        print("U heeft geen hoge hoed")
+    if not lichaamsgewicht_ok:
+        print("U voldoet niet aan onze gewicht eisen")
+    if not lengte_ok:
+        print("U voldoet niet aan onze lengte eisen")
+    if not certificaat_ok:
+        print("U heeft geen certificaat; overleven met gevaarlijk personeel")
+    if not dieren_dressur_ok:
+        print("U heeft niet genoeg ervaring in dieren-dressuur")
+    if not jongleren_ok:
+        print("U heeft niet genoeg ervaring in jongleren")
+    if not acrobatiek_ok:
+        print("U heeft genoeg ervaring in acrobatiek")
+    if not mbo_4_ok:
+        print("U heeft geen diploma mbo-4 ondernemen")
+    if not ondernemer_ok:
+        print("U bent niet lang genoeg werknemer")
+    if not werknemer_ok:
+        print("U heeft te weinig werknemers")
+    if not snor_ok:
+        print("Uw snor is niet breed genoeg")
+    if not haar_kleur_ok:
+        print("U heeft niet de juiste haarkleur")
+    if not haar_lengte_ok:
+        print("U heeft niet de juiste haar lengte")
+    if not glimlach_ok:
+        print("Uw glimlach is niet breed genoeg")
