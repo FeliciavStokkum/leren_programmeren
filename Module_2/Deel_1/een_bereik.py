12027b1c16f6add0c3dae3f55740e7b685f93366
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-11-10 12:04:43 +0100

    een_bereik.py

---
+++ Module_2/Deel_1/een_bereik.py
@@ -0,0 +1,6 @@
+#Vraag de gebruiker een getal in te voeren
+getal = int(input("Voer een getal in, waarvan je de tafel wilt weten. "))
+
+for i in range(1, 11): 
+    resultaat = getal * i #Hiermee wordt het getal keer de i gedaan
+    print(f"{i} X {getal} = {resultaat}") #Hiermee wordt het getal geprint inclusief de berekening
