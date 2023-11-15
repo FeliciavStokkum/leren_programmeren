3a244ce625dd218cf36d0bc0c6d47ff3cbdd8cf9
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-11-15 13:23:16 +0100

    raketlancering

---
+++ Module_2/Deel_1/raketlancering/raketlancering.py
@@ -0,0 +1,12 @@
+# Opdracht
+# Beschrijving van de opdracht verbergen
+# Maak een programma met een for loop die vanaf 30 terugtelt tot de raketlancering.
+
+# Print elke tel en print de lancering.
+
+# Doe een commit
+
+for getal in range(30, 0, -1):
+    print(getal)
+
+print("\nLancering!")
