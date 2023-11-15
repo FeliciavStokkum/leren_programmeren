70ccdadd93221d6f879f8793c9bdb911c2143307
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2023-11-15 13:47:11 +0100

    uren_dag

---
+++ Module_2/Deel_1/uren_dag.py
@@ -0,0 +1,11 @@
+# Maak een programma met één (1) for loop die de hele uren van de dag print (2x12).
+
+# (Geneste for loops mogen wel, geen 2 aparte for loops)
+
+# Voor de ochtend voeg je 'AM' toe. Voor de middag voeg je 'PM' toe
+
+for ochtend in range(1, 13):
+    print(f"{ochtend} AM")
+    if ochtend == 12:
+        for middag in range(13, 25):
+            print(f"{middag} PM")
