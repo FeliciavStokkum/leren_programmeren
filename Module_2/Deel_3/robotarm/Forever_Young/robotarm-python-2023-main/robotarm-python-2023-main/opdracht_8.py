3c90a7ce416de76e9423861b80a4ee6106a9b8fe
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-02-14 12:07:43 +0100

    opdracht_8 robotarm

--- Module_2/Deel_3/robotarm/Forever_Young/robotarm-python-2023-main/robotarm-python-2023-main/opdracht_8.py
+++ Module_2/Deel_3/robotarm/Forever_Young/robotarm-python-2023-main/robotarm-python-2023-main/opdracht_8.py
@@ -2,7 +2,14 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 8')
 
 # Jouw python instructies zet je vanaf hier:
-
+for a in range(7):
+    robotArm.moveRight()
+    robotArm.grab()
+    for i in range(8):  
+        robotArm.moveRight()
+    robotArm.drop()
+    for b in range(9):
+        robotArm.moveLeft()
 
 # Na jouw code wachten tot het sluiten van de window:
-robotArm.wait()
+robotArm.wait() 
