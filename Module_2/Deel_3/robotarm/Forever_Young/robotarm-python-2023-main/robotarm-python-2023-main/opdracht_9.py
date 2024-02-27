117837ab6ad3b31ff26a00f4f350bcdcaee6f0d7
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-02-27 14:08:08 +0100

    opdracht_9

--- Module_2/Deel_3/robotarm/Forever_Young/robotarm-python-2023-main/robotarm-python-2023-main/opdracht_9.py
+++ Module_2/Deel_3/robotarm/Forever_Young/robotarm-python-2023-main/robotarm-python-2023-main/opdracht_9.py
@@ -1,42 +1,16 @@
 from RobotArm import RobotArm
 robotArm = RobotArm('exercise 9')
-# # Jouw python instructies zet je vanaf hier:
-for i in range(1):
-    robotArm.grab()
-    for a in range(5):
-        robotArm.moveRight()
-    robotArm.drop()
-    for b in range(4):
-        robotArm.moveLeft()
-    robotArm.grab()
-    for c in range(5):
-        robotArm.moveRight()
-    robotArm.drop()
-    for d in range(5):
-        robotArm.moveLeft()
-    robotArm.grab()
-    for e in range(5):
-        robotArm.moveRight()
-    robotArm.drop()
-    for f in range(4):
-        robotArm.moveLeft()
-    robotArm.grab()
-    for g in range(5):
-        robotArm.moveRight()
-    robotArm.drop()
-    for h in range(5):
-        robotArm.moveLeft()
+
+aantal = 5
+for x in range(1, 11):
     robotArm.grab()
     for i in range(5):
         robotArm.moveRight()
     robotArm.drop()
-    for j in range(5):
-        robotArm.moveLeft()
-    robotArm.grab()
-    for k in range(5):
-        robotArm.moveRight()
-    robotArm.drop()
+    if x == 1 or x == 3 or x == 6:
+        aantal = 4
+    else:
+        aantal = 5
+    for i in range(aantal): robotArm.moveLeft()
 
-
-# # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
