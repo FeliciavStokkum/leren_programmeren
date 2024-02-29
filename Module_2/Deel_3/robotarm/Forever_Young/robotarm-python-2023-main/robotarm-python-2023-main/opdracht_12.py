fd2be89e1fce4b3c3da589ffd7fd89310211a471
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-02-29 17:02:39 +0100

    opdracht_12

--- Module_2/Deel_3/robotarm/Forever_Young/robotarm-python-2023-main/robotarm-python-2023-main/opdracht_12.py
+++ Module_2/Deel_3/robotarm/Forever_Young/robotarm-python-2023-main/robotarm-python-2023-main/opdracht_12.py
@@ -1,15 +1,46 @@
+# from RobotArm import RobotArm
+# robotArm = RobotArm('exercise 12')
+
+# while True:
+#     robotArm.grab()
+#     robotArm.scan()
+#     color = robotArm.scan()
+#     if color == "red":
+#             robotArm.moveRight()
+#     else:
+#         robotArm.drop()
+#         robotArm.moveRight()
+
+# robotArm.wait()
+
+
 from RobotArm import RobotArm
 robotArm = RobotArm('exercise 12')
+
+stappen_links = 8
+stappen_rechts = 1
+
+for c in range(8):
+    robotArm.moveRight()
 
 while True:
     robotArm.grab()
     robotArm.scan()
     color = robotArm.scan()
     if color == "red":
-        for x in range():
+        for x in range(stappen_rechts):
             robotArm.moveRight()
+        robotArm.drop()
+        for x in range(stappen_rechts):
+            robotArm.moveLeft()
     else:
         robotArm.drop()
-        robotArm.moveRight()
+
+    if stappen_links > 0:
+        robotArm.moveLeft()
+        stappen_rechts += 1
+        stappen_links -= 1
+    elif stappen_links == 0:
+        break
 
 robotArm.wait()
