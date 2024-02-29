7411452147c6dcc6a70717a437e80765da2a5bcc
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-02-29 17:30:01 +0100

    opdracht_12

--- Module_2/Deel_3/robotarm/Forever_Young/robotarm-python-2023-main/robotarm-python-2023-main/opdracht_12.py
+++ Module_2/Deel_3/robotarm/Forever_Young/robotarm-python-2023-main/robotarm-python-2023-main/opdracht_12.py
@@ -1,19 +1,3 @@
-# from RobotArm import RobotArm
-# robotArm = RobotArm('exercise 12')
-
-# while True:
-#     robotArm.grab()
-#     robotArm.scan()
-#     color = robotArm.scan()
-#     if color == "red":
-#             robotArm.moveRight()
-#     else:
-#         robotArm.drop()
-#         robotArm.moveRight()
-
-# robotArm.wait()
-
-
 from RobotArm import RobotArm
 robotArm = RobotArm('exercise 12')
 
