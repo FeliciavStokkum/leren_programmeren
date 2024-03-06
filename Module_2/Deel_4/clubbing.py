5724af7dd36ca5661582e7c4d2b96bc05085d764
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-03-06 17:45:06 +0100

    clubbing.py

--- Module_2/Deel_4/clubbing.py
+++ Module_2/Deel_4/clubbing.py
@@ -4,23 +4,56 @@ PRIJS_CHAMPAGNE = 12.30
 
 DRANKJES = ('cola', 'bier', 'champagne')
 VIP_LIST = ('jeroen', 'jouke', 'rudi')
-drinken = ("Wat wil je drinken? ")
-bandje = ("Heb je een bandje Y/N? ")
 
-#bouw hieronder de floowchart na
+drinken = "Wat wil je drinken? "
+bandje = ""
+complimenten = "Alstublieft, complimenten van het huis"
 leeftijd_input = int(input("Wat is je leeftijd? "))
 naam_vraag = "Wat is je naam? "
 drinken_vraag = "Wat wil je drinken? "
-aantal_jaar_18 = 18 - leeftijd_input
 aantal_jaar_21 = 21 - leeftijd_input
+aantal_jaar_18 = 18 - leeftijd_input
 
 if leeftijd_input < 18:
-    print(f"Sorry je mag niet naar binnen.Je mag over {aantal_jaar_18} jaar naar binnen.")
-elif leeftijd_input > 18:
+    print(f"Sorry je mag niet naar binnen. Probeer het in {aantal_jaar_18} jaar nog eens.")
+else:  
     naam_input = input(naam_vraag)
-    if naam_input in VIP_LIST and leeftijd_input >= 21:
-        bandje = "blauw"
+    if naam_input.lower() in VIP_LIST: 
+        if leeftijd_input >= 21:
+            bandje = "blauw"
+        else:
+            bandje = "rood"
         print(f"Je krijgt een {bandje} bandje")
-    elif naam_input not in VIP_LIST and leeftijd_input >= 21:
-        bandje = "rood"
-        print(f"Je krijgt een {bandje} bandje")
+    else:
+        if leeftijd_input >= 21:
+            stempel = True
+            print("Je krijgt een stempel.")
+    
+    drinken_input = input(drinken_vraag).lower() 
+    if drinken_input in DRANKJES:
+        if drinken_input == "cola":
+            if bandje:
+                print(complimenten)
+            else:
+                print(f"Alsjeblieft je cola, dat is dan €{PRIJS_COLA}.")
+
+        elif drinken_input == "bier":
+            if leeftijd_input < 21:
+                aantal_jaar_21 = 21 - leeftijd_input
+                print(f"Sorry geen alcohol voor jou. Kom over {aantal_jaar_21} jaar terug")
+            else:
+                if bandje == "blauw" or bandje == "rood":
+                    print(complimenten)
+                elif stempel:
+                    print(f"Alsjeblieft je {DRANKJES[1]}. Dat is dan €{PRIJS_BIER}.")
+        
+        elif drinken_input == "champagne":
+            if leeftijd_input < 21:
+                print(f"Sorry geen alcohol voor jou. Kom over {aantal_jaar_21} jaar terug")
+            else:
+                if bandje == "blauw":
+                    print(f"Alsjeblieft je champagne. Dat is dan €{PRIJS_CHAMPAGNE}.")
+                else:
+                    print("Sorry, alleen vips mogen champagne bestellen.")
+    else:
+        print("Sorry geen idee wat je bedoeld, hier een glaasje water. ")
