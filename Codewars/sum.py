731f49c819c03ce0b8946a1763b51aa9f46512b0
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-03-19 14:09:54 +0100

    codewars

--- Codewars/sum.py
+++ Codewars/sum.py
@@ -1,7 +1,11 @@
 def sum_array(arr):
+    #Opschonen van je input!
     if arr is None:
         return 0
-    arr.remove(max(arr))
+    if len(arr) < 3:
+        return 0
     arr.remove(min(arr))
+    arr.remove(max(arr))
     return sum(arr)
-print(sum_array([6, 2, 1, 8, 10]))
+
+print(sum_array([1, 2, 17]))
