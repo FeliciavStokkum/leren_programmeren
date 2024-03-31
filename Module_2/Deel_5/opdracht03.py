2bcd9202a64f82ae9f4ff722f37f09baa6aa3f6f
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-03-31 21:09:35 +0200

    opdracht03

---
+++ Module_2/Deel_5/opdracht03.py
@@ -0,0 +1,5 @@
+#Print alleen de namen van al het fruit in de fruitmand met maximaal 3 regels code (minus lege regels en de import).
+import fruitmand
+
+for fruit in fruitmand.fruitmand:
+    print(fruit["name"])
