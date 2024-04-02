0837035462b71acebb7301f459ac6b11173ac2b7
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-04-02 13:39:27 +0200

    opdracht02

--- Module_2/Deel_5/opdracht02.py
+++ Module_2/Deel_5/opdracht02.py
@@ -1,13 +1,13 @@
 #Print alleen het totaal aantal stuks fruit in de fruitmand, probeer dit zo kort mogelijk te doen.
-import fruitmand
+from fruitmand import *
 
 #Manier 1
 lijst = [] #lege lijst, zodat je elk fruitstuk uit de lijst kan halen
 
-for fruit in fruitmand.fruitmand: #for loop
+for fruit in fruitmand: #for loop
     lijst.append(fruit) #hier word in de lijst de fruitsoorten geappend
 
 print(len(lijst)) #Hier print ik de lengte van de lijst 
 
 #Manier 2
-print(len(fruitmand.fruitmand)) #hij print de hoeveelheid items in fruitmand
+print(len(fruitmand)) #hij print de hoeveelheid items in fruitmand
