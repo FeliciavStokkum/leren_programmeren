c0863d5a8b2c847beb4bd7efaf605f59b5344c4e
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-04-03 12:15:01 +0200

    opdracht08 bezig

---
+++ Module_2/Deel_5/opdracht08.py
@@ -0,0 +1,5 @@
+# Print alleen het gewicht van de volledige fruitmand.
+# Voeg daarna (met code) een watermeloen toe aan de fruitmand en print daarna alleen het gewicht van de volledige fruitmand.
+
+from fruitmand import *
+
