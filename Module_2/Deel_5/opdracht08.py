ac1d2b2ea5f2202041db42327d3b8b7e04525249
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-04-04 10:19:45 +0200

    opdracht08 klaar

--- Module_2/Deel_5/opdracht08.py
+++ Module_2/Deel_5/opdracht08.py
@@ -1,5 +1,21 @@
 # Print alleen het gewicht van de volledige fruitmand.
 # Voeg daarna (met code) een watermeloen toe aan de fruitmand en print daarna alleen het gewicht van de volledige fruitmand.
-
 from fruitmand import *
 
+#Functie om het gewicht in de fruitmand te berekenen
+def gewicht_fruitmand():
+    gewicht = 0
+    for fruit in fruitmand:
+        gewicht +=  fruit["weight"]
+    return(gewicht)
+print(gewicht_fruitmand())
+
+#Hier voeg ik een watermeloen toe aan de list
+watermeloen = {
+    'name' : 'watermeloen',
+    'weight' : 2500,
+    'color' : 'green',
+    'round' : True
+}
+fruitmand.append(watermeloen)
+print(gewicht_fruitmand())
