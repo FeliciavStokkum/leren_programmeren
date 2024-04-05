872aa85c94a23d8ef30e1a43114635bfa24b6b88
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-04-05 13:15:57 +0200

    opdracht09 klaar

--- Module_2/Deel_5/opdracht09.py
+++ Module_2/Deel_5/opdracht09.py
@@ -1,8 +1,13 @@
-# Verwijder (met code) de druif uit de fruitmand en print alleen alle verschillende kleuren in de fruitmand, in maximaal 8 regels code (minus lege regels en de import).
-
+# Verwijder (met code) de druif uit de fruitmand en print alleen alle verschillende kleuren in de fruitmand, 
+# in maximaal 8 regels code (minus lege regels en de import).
 from fruitmand import *
 
 for fruit in fruitmand:
-    if 'name' == 'druif':
-        fruitmand.pop(fruit)
-print(fruitmand)
+    if fruit["name"] == "druif":
+        fruitmand.remove(fruit)
+
+kleuren = []
+for kleur in fruitmand:
+    if kleur["color"] not in kleuren:
+        kleuren.append(kleur["color"])
+print(kleuren)
