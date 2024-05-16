7408977782b4b131607296e508b31407ebdaef3a
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-05-16 23:43:08 +0200

    lootjes_update

--- Module_2/proef/realisatie_lootjes.py
+++ Module_2/proef/realisatie_lootjes.py
@@ -1,32 +1,49 @@
-import random 
+import random
+
 namen_lijst = []
 
-nieuwe_naam = ("Wil je nog een naam invoeren? ja/nee: ")
-
 while True:
-    lengte_namen_lijst = len(namen_lijst) + 1
     naam_vraag = input("Voer een naam in: ")
 
     if naam_vraag not in namen_lijst:
         namen_lijst.append(naam_vraag)
-        print(f"Naam toegevoegd. Huidige namenlijst: {namen_lijst}")
+        print(f"Naam toegevoegd. Huidige namenlijst: {namen_lijst} \n")
     else:
         print("Naam staat al in de lijst. Probeer opnieuw.")
-        continue
 
-    if lengte_namen_lijst < 3: 
-        print(naam_vraag)
-    elif lengte_namen_lijst >= 3:
-        print(input(nieuwe_naam))
-        
-        if nieuwe_naam != "ja":
+    if len(namen_lijst) < 3: 
+        print("\n")
+    else:
+        while True:
+            nieuwe_naam = input("Wil je nog een naam invoeren? ja/nee: ")
+            if nieuwe_naam == "ja":
+                break
+            elif nieuwe_naam == "nee":
+                break
+            else:
+                print("Ongeldige invoer. Gebruik alleen 'ja' of 'nee'. Probeer opnieuw. \n")
+        if nieuwe_naam == "nee":
             break
-        else:
-            print("Ongeldige invoer. type 'ja' of 'nee'")
-            print(input(nieuwe_naam))
 
-#Hier word de lijst met namen geprint en het aantal dat in de lijst zit
-print(f"Eindlijst met namen: {namen_lijst}, er staan {lengte_namen_lijst} namen in de lijst")
+# Hier wordt de lijst met namen geprint en het aantal dat in de lijst zit
+print(f"Eindlijst met namen: {namen_lijst}, er staan {len(namen_lijst)} namen in de lijst \n")
 
-#Namen (lootjes) worden geschut
-random.shuffle(namen_lijst)
+# Namen (lootjes) worden geschud
+# random.shuffle(namen_lijst)
+# print(f"De namen zijn geschud: {namen_lijst}")
+
+# for naam in range(len(namen_lijst)):
+#     lootje_een = namen_lijst[naam]
+#     lootje_twee = namen_lijst[(naam + 1)]
+#     print(f"{lootje_een} heeft {lootje_twee}")
+
+#Random naam uit lijst halen
+#Controleren of gelijk is aan eerste naam
+#Niet gelijk print lootjes
+#Item dat hetzelfde was verwijderen
+
+for naam in namen_lijst:
+    lootje = random.choice(namen_lijst)
+    while lootje == naam:
+        lootje = random.choice(namen_lijst)
+    print(f"{naam} heeft {lootje}")
