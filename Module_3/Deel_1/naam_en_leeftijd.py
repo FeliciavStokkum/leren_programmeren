37dd6a2b82403f5d3a177c3b40153797c78830c2
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-05-30 11:40:34 +0200

    update naam_leeftijd

--- Module_3/Deel_1/naam_en_leeftijd.py
+++ Module_3/Deel_1/naam_en_leeftijd.py
@@ -1,10 +1,24 @@
 def naam_age():
-    dict_name_age = {}  
-    name = input("Wat is je naam?: ")  
-    age = input("Wat is je leeftijd?: ")  
-    dict_name_age['name'] = name  
-    dict_name_age['age'] = age 
+    #Nieuwe dict voor de name en age
+    dict_name_age = {} 
+    #De name en age worden opgeslagen in de dict
+    dict_name_age['name'] = input("Wat is je naam?: ")  
+    dict_name_age['age'] = input("Wat is je leeftijd?: ")  
     return dict_name_age 
 
-resultaat = naam_age()
-print(f"{resultaat['name']} is {resultaat['age']} jaar")
+def questions():
+    #Hier word een lege lijst gemaakt
+    lijst = []
+    #Zolang dit waar is gaat hij door
+    while True:
+        #Resultaat word uit de functie naam_age gehaald
+        resultaat = naam_age()
+        #Hier word dit resultaat in een lijst toegevoegd
+        lijst.append(resultaat)
+        if input("Toets enter om door te gaan of stop om te printen ").lower() == "stop":
+            return lijst
+            break
+
+lijst = questions()
+for dict in lijst:
+    print(f"{dict['name']} is {dict['age']} jaar")
