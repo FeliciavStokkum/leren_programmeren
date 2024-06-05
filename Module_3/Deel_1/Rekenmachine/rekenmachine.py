7b0a9951d28d9ccc7b59a6359abd06aee2596bf5
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-06-05 11:55:48 +0200

    update rekenmachine

--- Module_3/Deel_1/Rekenmachine/rekenmachine.py
+++ Module_3/Deel_1/Rekenmachine/rekenmachine.py
@@ -1,24 +1,34 @@
 from functions import *
 
 while True:
-    choice = input("Wat wilt u doen? \n A) getallen optellen \n B) getallen aftrekken \n C) getallen vermenigvuldigen \n D) getallen delen \n E) getal ophogen \n F) getal verlagen \n G) getal verdubbelen \n H) getal halveren \n Kies: ").lower()
-    number1 = input("Voer het eerste getal in: ")
-    number2 = input("Voer het tweede getal in: ")
-    if choice == "A":
+    while True:
+        choice = input("Wat wilt u doen? \n A) getallen optellen \n B) getallen aftrekken \n C) getallen vermenigvuldigen \n D) getallen delen \n E) getal ophogen \n F) getal verlagen \n G) getal verdubbelen \n H) getal halveren \n Kies: ").lower()
+        
+        if choice in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+            number1 = int(input("Wat is je eerste getal: "))
+            break 
+        else:
+            print("Dat is geen keuze, probeer opnieuw")
+
+    if choice == "a":
+        number2 = int(input(f"Welk getal wil je bij {number1} optellen: "))
         antwoord = addition(number1, number2)
-    elif choice == "B":
+    elif choice == "b":
+        number2 = int(input(f"Welk getal wil je van {number1} aftrekken: "))
         antwoord = subtraction(number1, number2)
-    elif choice == "C":
+    elif choice == "c":
+        number2 = int(input(f"Welk getal wil je met {number1} vermenigvuldigen: "))
         antwoord = multiplication(number1, number2)
-    elif choice == "D": 
+    elif choice == "d":
+        number2 = int(input(f"Met welk getal wil je {number1} delen: "))
         antwoord = division(number1, number2)
-    elif choice == "E":
-        antwoord = addition(number1, number2)
-    elif choice == "F":
-        antwoord = subtraction(number1, number2)
-    elif choice == "G":
-        antwoord = multiplication(number1, number2)
-    elif choice == "H":
-        antwoord = division(number1, number2)
-    break
-    print(antwoord)
+    elif choice == "e":
+        antwoord = addition(number1, 1)
+    elif choice == "f":
+        antwoord = subtraction(number1, 1)
+    elif choice == "g":
+        antwoord = multiplication(number1, 2)
+    elif choice == "h":
+        antwoord = division(number1, 2)
+    
+    print(f"Het antwoord is: {antwoord}")
